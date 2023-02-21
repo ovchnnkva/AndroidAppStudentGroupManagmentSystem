77fb0bbeb1a4bd76ba6f10bab45995a8e5bc781e
ovchnnkva <ov4innnickova@yandex.ru>
2023-02-21 21:21:33 +0300

    Create classes schedule for student

---
+++ app/src/main/java/com/hfad/studentsystemapp/StudentScheduleActivity.java
@@ -0,0 +1,14 @@
+package com.hfad.studentsystemapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_schedule);
+    }
+}
