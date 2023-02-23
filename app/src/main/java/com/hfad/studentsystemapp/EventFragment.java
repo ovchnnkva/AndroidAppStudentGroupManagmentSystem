8b6eab91563788604f0fa1fbbfa003a89e167308
ovchnnkva <ov4innnickova@yandex.ru>
2023-02-23 11:05:18 +0300

    Create event schedule activity

---
+++ app/src/main/java/com/hfad/studentsystemapp/EventFragment.java
@@ -0,0 +1,44 @@
+package com.hfad.studentsystemapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class EventFragment extends Fragment {
+    private String name;
+    private String date;
+    private String location;
+    EventFragment(String name, String date, String location){
+        this.name = name;
+        this.date = date;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ app/src/main/java/com/hfad/studentsystemapp/EventScheduleActivity.java
@@ -0,0 +1,28 @@
+package com.hfad.studentsystemapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class EventScheduleActivity extends FragmentActivity {
+    ArrayList<EventFragment> events = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_schedule);
+        initData();
+        RecyclerView recyclerView = findViewById(R.id.container);
+        EventAdapter adapter = new EventAdapter(this, events);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initData(){
+        events.add(new EventFragment("Собрание","03.03.2023 12:30", "ИВТиПТ а.514"));
+        events.add(new EventFragment("Ярмарка вакансий", "23.03.2023 11:55","а.514" ));
+    }
+}
